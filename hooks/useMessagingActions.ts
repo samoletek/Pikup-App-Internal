c705f7740571753aba2324aea74dda0276536bdb
Drew Sergienko <drew.sergienko@gmail.com>
2026-03-16 20:21:40 +0800

    chore: remove dead code and prune unused dependencies

--- hooks/useMessagingActions.ts
+++
@@ -1,5 +0,0 @@
-import { useMessagingActions as useMessagingActionsContext } from "../contexts/AuthContext";
-
-export const useMessagingActions = () => {
-  return useMessagingActionsContext();
-};
--- hooks/useTripActions.ts
+++
@@ -1,5 +0,0 @@
-import { useTripActions as useTripActionsContext } from "../contexts/AuthContext";
-
-export const useTripActions = () => {
-  return useTripActionsContext();
-};
