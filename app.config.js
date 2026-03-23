3abbab1a5fcc283e852caaa071afd3bf354f1046
Drew Sergienko <drew.sergienko@gmail.com>
2026-03-23 16:47:35 +0800

    chore(release): bump version to 1.0.12 (ios build 14, android 14)

--- app.config.js
+++ app.config.js
@@ -8,7 +8,7 @@ module.exports = {
   expo: {
     name: "pikup",
     slug: "pikup-app",
-    version: "1.0.11",
+    version: "1.0.12",
     scheme: "pikup",
     orientation: "portrait",
     icon: "./assets/icon.png",
@@ -22,7 +22,7 @@ module.exports = {
     assetBundlePatterns: ["**/*"],
 
     ios: {
-      buildNumber: "13",
+      buildNumber: "14",
       supportsTablet: true,
       usesAppleSignIn: true,
       appleTeamId: "99LU49ANLX",
@@ -64,7 +64,7 @@ module.exports = {
     },
 
     android: {
-      versionCode: 13,
+      versionCode: 14,
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#ffffff",
